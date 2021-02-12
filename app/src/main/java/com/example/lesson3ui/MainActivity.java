f9dce582ad3905a8c9dbe74c8c9ce12091fdce5b
Wyatt McComb <02wyatt.mccomb@gulllakecs.org>
2021-02-12 12:29:41 -0500

    Experimented with some math and code

--- app/src/main/java/com/example/lesson3ui/MainActivity.java
+++ app/src/main/java/com/example/lesson3ui/MainActivity.java
@@ -15,9 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // PASTE CODE YOU WANT TO TEST HERE
-        display1("This is Box 1.");
-        display2("And this is Box 2.");
-        display3("And look! Box 3!");
+        int weekday = 5;
+        int weekend = 9;
+        int optimalHours = 7 * 8;
+        int actualHours = weekday;
+        actualHours = actualHours + weekend * 2;
+        int solution = optimalHours - actualHours;
+        display(solution);
 
     }
 
@@ -29,11 +33,6 @@ public class MainActivity extends AppCompatActivity {
     public void display(String text) {
         TextView t = (TextView) findViewById(R.id.display_text_view);
         t.setText(text);
-    }
-
-    public void display(int text) {
-        TextView t = (TextView) findViewById(R.id.display_text_view);
-        t.setText(text + "");
     }
 
     public void display1(String text) {
@@ -48,5 +47,9 @@ public class MainActivity extends AppCompatActivity {
     public void display3(String text) {
         TextView t = (TextView) findViewById(R.id.display_text_view_3);
         t.setText(text);
+    }
+    public void display (int i) {
+        TextView t = (TextView) findViewById(R.id.display_text_view);
+        t.setText(""+i);
     }
 }
